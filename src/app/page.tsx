e67cb33232d9b49e5cb841b6d7bf8e96448a4c09
Maria Shmakova <masha.shmakova@gmail.com>
2025-04-30 13:50:34 +0300

    components were fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 // import Image from "next/image";
 
-import Background from "@/сomponents/Background";
-import Header from "@/сomponents/Header";
-import Hero from "@/сomponents/Hero";
+import Background from "@/components/Background";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
