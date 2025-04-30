7700052b8048a26543a00630bcc8259891bf69b0
Maria Shmakova <masha.shmakova@gmail.com>
2025-04-30 14:17:27 +0300

    fade in animation

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -3,7 +3,7 @@
 export default function Background() {
   return (
     <div
-      className="w-full bg-cover bg-left bg-no-repeat h-[700px] md:h-[1000px] 2xl:h-[1450px] "
+      className="w-full bg-cover bg-left bg-no-repeat h-[700px] md:h-[1000px] 2xl:h-[1450px] opacity-0 animate-fade-in"
       style={{ backgroundImage: `url('/bg_man.jpg')` }}
     >
       <div className="w-full h-full bg-white/40 backdrop-blur-sm" />
